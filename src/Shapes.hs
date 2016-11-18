c987861c5ff9eaac4d0911cc9f28144660818094
Jeremy <barischj@tcd.ie>
2016-11-18 17:14:15 +0000

    wording

--- src/Shapes.hs
+++ src/Shapes.hs
@@ -5,18 +5,18 @@ import qualified Data.Char as Ch
 -- |Example of a parsed `Drawing`.
 _example = read "[(Identity, Circle, [FillColor #123456])]" :: Drawing
 
--- |An RGB data type consisting of three `Int`s.
+-- |An RGB data type represented by three `Int`s.
 -- The instance `Read RGB` ensures the `Int`s are in range.
 data Rgb = Rgb Int Int Int
 
--- |Read an RGB value in the format #123456.
+-- |Read an `RGB` value in the format #123456.
 instance Read Rgb where
     readsPrec _ input = do
         ([h], r1:r2:g1:g2:b1:b2:rest) <- lex input
         [(Rgb (read [r1, r2]) (read [g1, g2]) (read [b1, b2]), rest)
          | h == '#' && all Ch.isDigit [r1, r2, g1, g2, b1, b2]]
 
--- |Show an RGB value in the format #123456.
+-- |Show an `RGB` value in the format #123456.
 instance Show Rgb where
     show (Rgb r g b) = "#" ++ concatMap show [r, g, b]
 

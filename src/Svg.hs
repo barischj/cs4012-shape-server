5faf1a0419d4abbed71bde38e53fe5b0ec71e6c2
Jeremy <barischj@tcd.ie>
2016-11-18 17:19:04 +0000

    comments

--- src/Svg.hs
+++ src/Svg.hs
@@ -8,15 +8,16 @@ import qualified Text.Blaze.Internal         as I
 import           Text.Blaze.Svg11            hiding (style)
 import qualified Text.Blaze.Svg11.Attributes as A
 
--- Useful for partially applying an argument to `!` in reverse order.
+-- |Useful for partially applying an argument to `!` in reverse order.
 apply = flip (!)
 
--- Apply a `Style` to an `Svg`. The `Style` is given as an
+-- |Applies a `Style` to an `Svg`. The `Style` is given as an
 -- `AttributeValue -> Attribute` e.g. `A.height` and the `AttributeValue` is
 -- derived from an instance of `Show` e.g. `5`.
 applyStyle :: Show a => (AttributeValue -> Attribute) -> a -> Svg -> Svg
 applyStyle ava a = apply $ ava $ I.stringValue $ show a
 
+-- |Returns an `Svg` with a `Style` applied.
 styleSvg :: Style -> Svg -> Svg
 styleSvg (FillColor   c) = applyStyle A.fill        c
 styleSvg (Height      h) = applyStyle A.height      h
@@ -24,6 +25,7 @@ styleSvg (StrokeColor c) = applyStyle A.stroke      c
 styleSvg (StrokeWidth w) = applyStyle A.strokeWidth w
 styleSvg (Width       w) = applyStyle A.width       w
 
+-- |Returns an `Svg` with a `Transform` applied.
 transformSvg :: Transform -> Svg -> Svg
 transformSvg Identity        = id
 transformSvg (Translate v)   = apply $ A.transform $ translate (getX v) (getY v)
@@ -31,6 +33,7 @@ transformSvg (Scale v)       = apply $ A.transform $ scale     (getX v) (getY v)
 transformSvg (Compose t1 t2) = transformSvg t2 . transformSvg t1
 transformSvg (Rotate a)      = apply $ A.transform $ rotate a
 
+-- |Returns the `Svg` corresponding to a `Shape`.
 shapeToSvg :: Shape -> Svg
 shapeToSvg Empty  = I.Empty
 shapeToSvg Circle = circle

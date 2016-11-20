d5ad0c8f57e93b6c1f3c9c63002cae090553bf1e
Jeremy <barischj@tcd.ie>
2016-11-20 18:35:37 +0000

    simplify Server.hs

--- src/Server.hs
+++ src/Server.hs
@@ -3,15 +3,10 @@
 module Server where
 
 import qualified Data.List.Utils as U
-import           Data.Text.Lazy
-import           Shapes          (Drawing)
 import           Svg             (drawingToSvg, renderSvg)
 import           Web.Scotty
 
 start = scotty 3000 $
     get "/:input" $ do
         input <- U.replace "%20" " " <$> param "input"
-        html $ response input
-
-response :: String -> Text
-response = renderSvg 500 500 . drawingToSvg . read
+        html $ renderSvg 500 500 $ drawingToSvg $ read input
